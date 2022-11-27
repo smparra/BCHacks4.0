2eb5d26723416be70af4324d840d7fdbc1fab960
Segundo Parra <segundoparra@Segundos-MacBook-Air.local>
2022-11-26 18:52:56 -0800

    Added Project

---
+++ NewBC/NewBC/NewBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewBCApp.swift
+//  NewBC
+//
+//  Created by Segundo Parra on 2022-11-26.
+//
+
+import SwiftUI
+
+@main
+struct NewBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
